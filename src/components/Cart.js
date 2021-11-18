8115ba5d070b59279d2b16d11e0fbec62d037c81
leo.schmal <leonardoschmal@gmail.com>
2021-11-18 00:04:24 -0300

    saco warnings

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,51 +2,61 @@ import { CartContext } from "../contexts/CartContext";
 import { useContext } from "react";
 import "./Cart.css";
 
+export const Cart = () => {
+  let pesosArg = Intl.NumberFormat("ar-AR", {
+    style: "currency",
+    currency: "ARG",
+  });
 
-export const Cart = ()=>{
-    let pesosArg = Intl.NumberFormat("ar-AR", {
-        style: "currency",
-        currency: "ARG",
-      });
+  const { cart, clearCart, removeItem } = useContext(CartContext);
 
-    const {cart, clearCart, removeItem} = useContext(CartContext);
-    
-    //aca usar useState y context
-    let total = 0;
-    let productos =0;
-    cart.forEach(function(a){total += parseInt(a.product.precio)*a.product.qty;});
-    cart.forEach(function(a){productos += parseInt(a.product.qty);});
-    
-    return(
-        <>
-        {/* <pre>
+  let total = 0;
+  cart.forEach(function (a) {
+    total += parseInt(a.product.precio) * a.product.qty;
+  });
+
+  return (
+    <>
+      {/* <pre>
             {JSON.stringify(cart, null, 2)}
         </pre> */}
 
-        <div className="cartProduct">
-            <div className="product">
-                    <p>Producto </p>
-                    <p>Cant.</p>
-                    <p>Precio U</p>
-                    <p>Subtotal</p>
-                    <p> id</p>
-            </div>
-        {cart !== [] ? cart.map((producto, index) => (            
+      <div className="cartProduct">
+        <div className="product">
+          <p>Producto </p>
+          <p>Cant.</p>
+          <p>Precio U</p>
+          <p>Subtotal</p>
+          <p> id</p>
+        </div>
+        {cart !== [] ? (
+          cart.map((producto, index) => (
             <div key={index}>
-                <div className="product">
-                    <p> {producto.product.nombre} </p>
-                    <p> {producto.product.qty}</p>
-                    <p>  ${pesosArg.format(producto.product.precio)}</p>
-                    <p>{producto.product.precio * producto.product.qty}</p>
-                    <p>  {producto.product.id}</p>
-                    <button onClick={()=>removeItem(producto.index)}>Eliminar</button>                
-                </div>                
-            </div>)) : <div>El carrito está vacío</div>}
-            <div>Total Compra: ${pesosArg.format(total)}               
+              <div className="product">
+                <p> {producto.product.nombre} </p>
+                <p> {producto.product.qty}</p>
+                <p> ${pesosArg.format(producto.product.precio)}</p>
+                <p>{producto.product.precio * producto.product.qty}</p>
+                <p> {producto.product.id}</p>
+                <button onClick={() => removeItem(producto.index)}>
+                  Eliminar
+                </button>
+              </div>
             </div>
-        {cart!== [] ? <><button onClick={clearCart}>Limpiar Carro</button><button>Terminar Compra</button></>: <p>na pa borrar</p>}
-        </div>   
-        </>
-    )
-
-}
+          ))
+        ) : (
+          <div>El carrito está vacío</div>
+        )}
+        <div>Total Compra: ${pesosArg.format(total)}</div>
+        {cart !== [] ? (
+          <>
+            <button onClick={clearCart}>Limpiar Carro</button>
+            <button>Terminar Compra</button>
+          </>
+        ) : (
+          <p>na pa borrar</p>
+        )}
+      </div>
+    </>
+  );
+};
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,49 +1,48 @@
-import { createContext , useState} from "react";
+import { createContext, useState } from "react";
 
 export const CartContext = createContext({});
 
-export const CartProvider =({children})=>{
-    const[cart, setCart]=useState([]);
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
 
-    const addItem = (items, item)=>{
-        if(isInCart(item)){  
-            cart.forEach(function(a){
-                if (a.product.id === items.product.id) {
-                    a.product.qty = a.product.qty + items.product.qty;
-                }
-            });
-                               
+  const addItem = (items, item) => {
+    if (isInCart(item)) {
+      cart.forEach(function (a) {
+        if (a.product.id === items.product.id) {
+          a.product.qty = a.product.qty + items.product.qty;
         }
-        else{
-            setCart([...cart,items]);
-            console.log('agregue nuevo');
-        }              
+      });
+    } else {
+      setCart([...cart, items]);
     }
+  };
 
-    function  removeItem (id) {        
-        const aux = Array.from(cart);
-        aux.splice(id, 1);              
-        setCart(aux);
+  function removeItem(id) {
+    const aux = Array.from(cart);
+    aux.splice(id, 1);
+    setCart(aux);
+  }
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const isInCart = (item) => {
+    const ttt = cart.map((ides) => {
+      return ides.product.id;
+    });
+    const uniqueId = [...new Set(ttt)];
+    if (uniqueId.includes(item.id)) {
+      return true;
+    } else {
+      return false;
     }
-
-    const clearCart = ()=>{        
-        setCart([]);        
-    }   
-
-    const isInCart = (item)=>{
-        const ttt = cart.map((ides)=>{
-            return ides.product.id;
-            });
-        const uniqueId= [...new Set(ttt)];        
-        if(uniqueId.includes(item.id)){             
-            return true;
-        }else{            
-            return false;
-        }
-    }
-    return(
-        <CartContext.Provider value={{cart, addItem, clearCart, removeItem, isInCart}}>
-            {children}
-        </CartContext.Provider>
-        )
-}
+  };
+  return (
+    <CartContext.Provider
+      value={{ cart, addItem, clearCart, removeItem, isInCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
